d65507ae18c567c8dd12aa8689d82362338c83cc
chuqdd <chuqdd@gmail.com>
2020-05-16 00:35:31 -0500

    !

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 var rn1 = Math.floor(Math.random()*6)+1;
 var rn2 = Math.floor(Math.random()*6)+1;
 
+var dice_l = document.querySelector(".dice_l");
+var dice_r = document.querySelector(".dice_r");
+
+var flag_l = document.querySelector(".flag_l");
+var flag_r = document.querySelector(".flag_r");
+
+
 if (rn1===1)
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice1.png");
+  dice_l.setAttribute("src", "images/dice1.png");
 } else if (rn1===2)
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice2.png");
+  dice_l.setAttribute("src", "images/dice2.png");
 } else if (rn1===3)
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice3.png");
+  dice_l.setAttribute("src", "images/dice3.png");
 } else if (rn1===4)
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice4.png");
+  dice_l.setAttribute("src", "images/dice4.png");
 } else if (rn1===5)
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice5.png");
+  dice_l.setAttribute("src", "images/dice5.png");
 } else
 {
-  document.querySelector(".dice_l").setAttribute("src", "images/dice6.png");
+  dice_l.setAttribute("src", "images/dice6.png");
 }
 
 
 if (rn2===1)
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice1.png");
+  dice_r.setAttribute("src", "images/dice1.png");
 } else if (rn2===2)
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice2.png");
+  dice_r.setAttribute("src", "images/dice2.png");
 } else if (rn2===3)
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice3.png");
+  dice_r.setAttribute("src", "images/dice3.png");
 } else if (rn2===4)
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice4.png");
+  dice_r.setAttribute("src", "images/dice4.png");
 } else if (rn2===5)
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice5.png");
+  dice_r.setAttribute("src", "images/dice5.png");
 } else
 {
-  document.querySelector(".dice_r").setAttribute("src", "images/dice6.png");
+  dice_r.setAttribute("src", "images/dice6.png");
 }
 
 
@@ -47,16 +54,16 @@ if (rn2===1)
 if (rn1===rn2)
 {
   document.querySelector("h1").innerHTML = "Draw! ";
-  document.getElementsByClassName("flag_l")[0].style.visibility="visible";
-  document.getElementsByClassName("flag_r")[0].style.visibility="visible";
+  flag_l.style.visibility="visible";
+  flag_r.style.visibility="visible";
 } else if (rn1>rn2)
 {
   document.querySelector("h1").innerHTML = "Player1 Win! ";
-  document.getElementsByClassName("flag_l")[0].style.visibility="visible";
-  document.getElementsByClassName("flag_r")[0].style.visibility="hidden";
+  flag_l.style.visibility="visible";
+  flag_r.style.visibility="hidden";
 } else
 {
   document.querySelector("h1").innerHTML = "Player2 Win! ";
-  document.getElementsByClassName("flag_l")[0].style.visibility="hidden";
-  document.getElementsByClassName("flag_r")[0].style.visibility="visible";
+  flag_l.style.visibility="hidden";
+  flag_r.style.visibility="visible";
 }
